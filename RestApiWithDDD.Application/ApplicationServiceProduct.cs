56523e4e12862aab0ee50d60c8e2f01a6cec9643
Diego Harrison <diegoharrisonsm@gmail.com>
2022-06-10 00:08:46 -0300

    feat: implementando restante das camadas e configuração ioc

---
+++ RestApiWithDDD.Application/ApplicationServiceProduct.cs
@@ -0,0 +1,46 @@
+﻿using RestApiWithDDD.Application.Dtos;
+using RestApiWithDDD.Application.Interfaces;
+using RestApiWithDDD.Application.Interfaces.Mappers;
+using RestApiWithDDD.Domain.Core.Interfaces.Services;
+
+public class ApplicationServiceProduct : IApplicationServiceProduct
+{
+    private readonly IServiceProduct serviceProduct;
+    private readonly IMapperProduct mapperProduct;
+
+    public ApplicationServiceProduct(IServiceProduct serviceProduct, IMapperProduct mapperProduct)
+    {
+        this.serviceProduct = serviceProduct;
+        this.mapperProduct = mapperProduct;
+    }
+
+    public void Add(ProductDto productDto)
+    {
+        var product = mapperProduct.MapperDtoToEntity(productDto);
+        serviceProduct.Add(product);
+    }
+
+    public IEnumerable<ProductDto> GetAll()
+    {
+        var products = serviceProduct.GetAll();
+        return mapperProduct.MapperListProductDto(products);
+    }
+
+    public ProductDto GetById(int id)
+    {
+        var product = serviceProduct.GetById(id);
+        return mapperProduct.MapperEntityToDto(product);
+    }
+
+    public void Remove(ProductDto productDto)
+    {
+        var product = mapperProduct.MapperDtoToEntity(productDto);
+        serviceProduct.Remove(product);
+    }   
+
+    public void Update(ProductDto productDto)
+    {
+        var product = mapperProduct.MapperDtoToEntity(productDto);
+        serviceProduct.Update(product);
+    }
+}
---
+++ RestApiWithDDD.Domain.Infrastructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using RestApiWithDDD.Application.Interfaces;
+using RestApiWithDDD.Application.Interfaces.Mappers;
+using RestApiWithDDD.Application.Mappers;
+using RestApiWithDDD.Domain.Core.Interfaces.Services;
+using RestApiWithDDD.Domain.Infrastructure.Data.Repositorys;
+using RestApiWithDDD.Domain.Services;
+
+namespace RestApiWithDDD.Domain.Infrastructure.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+
+            builder.RegisterType<ApplicationServiceClient>().As<IApplicationServiceClient>();
+            builder.RegisterType<ApplicationServiceProduct>().As<IApplicationServiceProduct>();
+            builder.RegisterType<ServiceClient>().As<IServiceClient>();
+            builder.RegisterType<ServiceProduct>().As<IServiceProduct>();
+            builder.RegisterType<RepositoryClient>().As<IRepositoryClient>();
+            builder.RegisterType<RepositoryProduct>().As<IRepositoryProduct>();
+            builder.RegisterType<MapperClient>().As<IMapperClient>();
+            builder.RegisterType<MapperProduct>().As<IMapperProduct>();
+
+            #endregion
+        }
+    }
+}
